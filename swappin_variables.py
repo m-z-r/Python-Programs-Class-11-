696c1e2a236bfabff772c79aa61913fdd88d1919
Maryam Zahid <xahidmariam@gmail.com>
2025-10-25 16:51:49 +0500

    Create swappin_variables.py

---
+++ swappin_variables.py
@@ -0,0 +1,16 @@
+# Activity: 1
+# Swap two variables without using a temporary variable
+
+num_1 = 5
+num_2 = 3
+
+print("First value = ", num_1, " & Second value = ", num_2)
+
+#usage of tuples
+num_1,num_2 = num_2,num_1
+
+#tuples allow easy switch-up
+print("After swapping, First value = ",num_1, " & Second value = ", num_2)
+
+# For more information regarding Python Variables and Data Structures, visit the following link:
+# Blog:  https://techopulse.com/is-this-the-way-to-learn-python-variables
